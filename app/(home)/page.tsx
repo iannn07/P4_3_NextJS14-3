4be25299e126af53ac0e5abe712d8a50c5bce3f7
Iannn <pristianb007@gmail.com>
2024-04-22 21:48:43 +0700

    Root Path

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 export default function Home() {
-	return (
-		<div>
-			<h1>Build Dashboard with role access using Next.js + Supabase </h1>
-		</div>
-	);
+  return (
+    <div className='flex flex-col justify-center items-center h-screen'>
+      <Image
+        src='/Home.png'
+        alt='Next.js Logo'
+        className='mb-10 rounded-xl'
+        width={800}
+        height={800}
+        priority
+      />
+      <Link href='/auth'>Click here to the app</Link>
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,55 +1,57 @@
-import type { Metadata } from "next";
-import { Jost } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/toaster";
+import type { Metadata } from 'next';
+import { Jost } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from '@/components/theme-provider';
+import { Toaster } from '@/components/ui/toaster';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const jost = Jost({
-	subsets: ["latin"],
-	weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '500', '700', '900'],
 });
 
 export const metadata: Metadata = {
-	metadataBase: new URL("https://daily-todo-task.vercel.app/"),
+  metadataBase: new URL('https://daily-todo-task.vercel.app/'),
 
-	title: {
-		template: "%s | Daily Todo",
-		default: "Daily Todo",
-	},
-	authors: {
-		name: "chensokheng",
-	},
-	description:
-		"Build dashboard with role managemanet using next.js and supabase.",
-	openGraph: {
-		title: "Daily Todo",
-		description: "Build dashboard with next.js and supabase ",
-		url: "https://daily-todo-task.vercel.app/",
-		siteName: "Daily Todo",
-		images: "/og.png",
-		type: "website",
-	},
-	keywords: ["daily web coding", "chensokheng", "dailywebcoding"],
+  title: {
+    template: '%s | Daily Todo',
+    default: 'Daily Todo',
+  },
+  authors: {
+    name: 'chensokheng',
+  },
+  description:
+    'Build dashboard with role managemanet using next.js and supabase.',
+  openGraph: {
+    title: 'Daily Todo',
+    description: 'Build dashboard with next.js and supabase ',
+    url: 'https://daily-todo-task.vercel.app/',
+    siteName: 'Daily Todo',
+    images: '/og.png',
+    type: 'website',
+  },
+  keywords: ['daily web coding', 'chensokheng', 'dailywebcoding'],
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body className={`${jost.className} antialiased dark:bg-[#09090B]`}>
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="dark"
-					enableSystem
-					disableTransitionOnChange
-				>
-					<main className="">{children}</main>
-					<Toaster />
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${jost.className} antialiased dark:bg-[#09090B]`}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='dark'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className=''>{children}</main>
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
